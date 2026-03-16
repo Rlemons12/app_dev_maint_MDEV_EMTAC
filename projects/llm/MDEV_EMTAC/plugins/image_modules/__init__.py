021bce3e0397f49146f26ae6021ede19f97d4f65
Robert C. Lemons <kermitsrocket@gmail.com>
2026-03-16 13:57:45 -0500

    initial commit
    Signed-off-by: Robert C. Lemons <kermitsrocket@gmail.com>

--- projects/llm/MDEV_EMTAC/plugins/image_modules/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-from .image_models import BaseImageModelHandler, CLIPModelHandler, NoImageModel, get_image_model_handler
-from modules.configuration.config import DATABASE_URL, ALLOWED_EXTENSIONS
-
-__all__ = ["BaseImageModelHandler", "CLIPModelHandler", "NoImageModel", "DATABASE_URL", "ALLOWED_EXTENSIONS"]
