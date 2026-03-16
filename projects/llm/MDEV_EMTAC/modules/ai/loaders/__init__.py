7c4e807869499ed97754dd3f9c996b1ec23d0910
Robert C. Lemons <kermitsrocket@gmail.com>
2026-03-16 14:00:42 -0500

    initial commit
    Signed-off-by: Robert C. Lemons <kermitsrocket@gmail.com>

---
+++ projects/llm/MDEV_EMTAC/modules/ai/loaders/__init__.py
@@ -0,0 +1,11 @@
+from .model_loader import (
+    load_ai_model,
+    load_embedding_model,
+    load_image_model,
+)
+
+__all__ = [
+    "load_ai_model",
+    "load_embedding_model",
+    "load_image_model",
+]
