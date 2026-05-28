d1596d06d857ba49412eb135746aea92ec6aae04
Robert C. Lemons <kermitsrocket@gmail.com>
2026-05-28 13:55:17 -0500

    Disable auto-reload in service dashboard run script
    Signed-off-by: Robert C. Lemons <kermitsrocket@gmail.com>

--- services/service_dashboard/run.py
+++ services/service_dashboard/run.py
@@ -1,9 +1,11 @@
 from app.main import app
 from configuration.config import SERVICE_DASHBOARD_HOST, SERVICE_DASHBOARD_PORT
 
+
 if __name__ == "__main__":
     app.run(
         debug=True,
+        use_reloader=False,
         host=SERVICE_DASHBOARD_HOST,
         port=SERVICE_DASHBOARD_PORT,
     )
